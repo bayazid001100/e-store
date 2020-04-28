f6cb58937f6a4cff391cdf8a95e10ef92b43a289
bayazid001100 <34455165+bayazid001100@users.noreply.github.com>
2020-04-29 00:52:01 +0600

    ProductList Component
    Product page layout

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ export const ButtonContainer = styled.button`
 text-transform: capitalize;
 font-size: 1.4rem;
 background: transparent;
-border: 0.05rem solid var(--lightBlue)
+border: 0.05rem solid var(--lightBlue);
 color: var(--lightBlue);
 border-radius: 0.5rem;
 padding: 0.2rem 0.5rem;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import Product from './Product'
-
+import Title from './Title'
 export default class ProductList extends Component {
+    state={
+        products:[]
+    }
     render() {
         return (
-            <div>
-                <Product/>
-            </div>
+            <React.Fragment>
+                <div className="py-5">
+                <div className="container">
+                {/*Product ROw*/}
+                <Title name="our" title="products"/>
+                    <div className="row"/>
+                </div>
+                </div>
+            </React.Fragment>
+            //<Product/>
         )
     }
 }
-
